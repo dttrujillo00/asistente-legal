76507e423727b755f65469e9c25835e0bc82c4d5
Daniel Tamayo <dttrujillo00@gmail.com>
2024-01-31 11:17:31 -0800

    envio de correo electrónico funcionando

--- src/components/EmailForm/EmailForm.jsx
+++ src/components/EmailForm/EmailForm.jsx
@@ -1,20 +1,37 @@
+import { useRef } from 'react'
+import emailjs from '@emailjs/browser'
 import { TitleSection } from '../TitleSection/TitleSection'
 import './EmailForm.css'
 
 export const EmailForm = () => {
+
+  const form = useRef();
+
+  const sendEmail = event => {
+    event.preventDefault();
+
+    emailjs.sendForm('service_kaurhwp', 'contact_form', form.current, '6vBOL4XQ5xIp85Xis')
+    .then( (result) => {
+      console.log(result.text);
+    }, (error) => {
+      console.log(error.text)
+    } );
+  };
+
   return (
     <>
       <TitleSection id={ 'inscriptionContainer' } title={ 'Inscripción' } />
+
       <div className="row row-cols-1 row-cols-md-2 m-auto container mb-3">
-        <form className='row g-3 border rounded-3 p-3 shadow-sm'>
+        <form ref={form} onSubmit={ sendEmail } className='row g-3 border rounded-3 p-3 shadow-sm'>
           <div className="col-md-6">
-            <input type="text" className="form-control" placeholder="Nombre completo" />
+            <input name='user_name' type="text" className="form-control" placeholder="Nombre completo" />
           </div>
           <div className="col-md-6">
-            <input type="email" className="form-control" placeholder="correo electrónico" />
+            <input name='user_email' type="email" className="form-control" placeholder="correo electrónico" />
           </div>
           <div className="col-12">
-            <textarea rows="5" className="form-control" placeholder="Mensaje"></textarea>
+            <textarea name='message' rows="5" className="form-control" placeholder="Mensaje"></textarea>
           </div>
           <div className="col-12">
             <button type="submit" className="btn btn-dark w-100">Enviar</button>
